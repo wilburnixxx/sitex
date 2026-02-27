d30e24f911dc5bbd2c698060c95bc06b001569d4
wilburnixxx <nikitapirogov127@gmail.com>
2026-02-27 12:01:34 +0500

    added analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'NikitaPirogov | Дизайн со смыслом',
@@ -29,6 +30,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <Analytics /> 
       </body>
     </html>
   );
